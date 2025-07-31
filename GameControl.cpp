869080f428f9081634dc508f831851e2b5031521
Karam alhaj <karam7550@gmail.com>
2025-08-01 00:16:40 +0300

    ported the code from linux to windows

--- GameControl.cpp
+++ GameControl.cpp
@@ -8,57 +8,12 @@
 #include <fcntl.h>
 #include "Snake.cpp"
 using namespace std;
+#include <conio.h>
 
 
 
-char getch() {
-    char buf = 0;
-    struct termios old = {0};
 
-    // Get current terminal settings
-    if (tcgetattr(STDIN_FILENO, &old) < 0)
-        perror("tcsetattr()");
 
-    // Set terminal to raw mode
-    old.c_lflag &= ~ICANON; // disable buffering
-    old.c_lflag &= ~ECHO;   // disable echo
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &old) < 0)
-        perror("tcsetattr ICANON");
-
-    // Read 1 character
-    if (read(STDIN_FILENO, &buf, 1) < 0)
-        perror("read()");
-
-    // Restore old settings
-    old.c_lflag |= ICANON;
-    old.c_lflag |= ECHO;
-    if (tcsetattr(STDIN_FILENO, TCSADRAIN, &old) < 0)
-        perror("tcsetattr ~ICANON");
-
-    return buf;// Save old terminal settings
-    termios oldt;
-    tcgetattr(STDIN_FILENO, &oldt);
-
-    // Turn off buffering and echo
-    termios newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    // Set non-blocking read
-    int oldf = fcntl(STDIN_FILENO, F_GETFL);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    char ch = 0;
-    if (read(STDIN_FILENO, &ch, 1) < 0) {
-        ch = 0;  // No key pressed
-    }
-
-    // Restore settings
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    return ch;
-}
 
 class GameControl{
 
@@ -79,7 +34,7 @@ class GameControl{
                                 cout<<'#';
                             }
                         }
-                        
+
                         if (i==0 || i==10 || j==0 || j==20){
                             cout<<'*';
                         }
@@ -104,7 +59,7 @@ class GameControl{
                 }
                snake->move(pressed,collision);
                
-                system("clear");
+                system("cls");
             }
         }
 
